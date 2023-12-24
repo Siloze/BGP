2e3bbf992ed8214a7abfc479c12dbdfcdd74ca3d
mbastard <87230146+mbastard42@users.noreply.github.com>
2023-12-24 06:57:51 +0100

    on dit oui enfait

--- P1/bgpd/config.sh
+++ P1/bgpd/config.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-echo net.ipv4.ip_forward=1 | tee -a /etc/sysctl.conf && sysctl -p
-# touch /etc/quagga/zebra.conf /etc/quagga/bgpd.conf /etc/quagga/ospfd.conf
-# chown quagga.quaggaavty /etc/quagga/*.conf
-# chmod 640 /etc/quagga/*.conf
-# sed -i 's/^bgpd=no$/bgpd=yes/' /etc/quagga/daemons
-# sed -i 's/^ospfd=no$/ospfd=yes/' /etc/quagga/daemons
-# sed -i 's/^ospf6d=yes$/ospf6d=no/' /etc/quagga/daemons
-# sed -i 's/^ripd=yes$/ripd=no/' /etc/quagga/daemons
-# sed -i 's/^ripngd=yes$/ripngd=no/' /etc/quagga/daemons
-# /etc/init.d/quagga restart
-while true
+# CONFIGURE FRROUTING (https://docs.frrouting.org/en/latest/setup.html)
+sed -i 's/^bgpd=no$/bgpd=yes/' /etc/frr/daemons
+sed -i 's/^ospfd=no$/ospfd=yes/' /etc/frr/daemons
+sed -i 's/^isisd=no$/isisd=yes/' /etc/frr/daemons
 
+tail -f /dev/null
